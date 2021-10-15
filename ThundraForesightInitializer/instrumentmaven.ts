15bca9eeaf5c76a040beb2e03a660dd01ce703a2
Yusuf Erdem <yusuf@thundra.io>
2021-10-15 19:00:30 +0300

    fix macos and windows problem

--- ThundraForesightInitializer/instrumentmaven.ts
+++ ThundraForesightInitializer/instrumentmaven.ts
@@ -24,24 +24,35 @@ export async function instrumentmaven(instrumenter_version?: string, agentPath?:
     console.log(`> Successfully downloaded the maven instrumentater to ${mvnInstrumentaterPath}`)
 
     console.log('> Updating pom.xml...')
-    const fs = require('fs');
 
-    let poms = '';
-    fs.readdirSync(process.cwd()).forEach(file => {
-        if (file.includes("pom.xml")) {
-            poms += file
-            poms += " "
-        }
-    });
+    let poms = await getPomFiles(process.cwd(), undefined);
 
     console.log('>found pom files: ' + poms.toString())
 
-    if (poms && poms.trim()) {
-        await execPromise(`"java -jar ${mvnInstrumentaterPath} ${agentPath} \\"${poms.trim()}\\""`)
+    if (poms && poms.length > 0) {
+        await execPromise(`java -jar ${mvnInstrumentaterPath} ${agentPath} "${poms.trim()}`)
         console.log('> Update to pom.xml is done')
     } else {
         console.warn("> Couldn't find any pom.xml files. Exiting the instrumentation step.")
     }
+}
+
+async function getPomFiles (dir, files_){
+    const fs = require('fs');
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    console.log("osman")
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getPomFiles(name, files_);
+        } else {
+            if (name.includes("pom.xml"))
+                files_.push(name);
+        }
+    }
+    console.log(files_)
+    return files_;
 }
 
 async function execPromise(command):Promise<string> {
